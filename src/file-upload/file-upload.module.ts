649c11957969b6983bfd89c124594cbafb5b1cc4
Emediong-Etuk <emedionggregory12@gmail.com>
2024-08-22 17:26:08 +0100

    file-upload changes

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { ImgEntity } from 'src/entity/file.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports:[MulterModule.register({
     dest: './upload',
-  })],
+  }), TypeOrmModule.forFeature([ImgEntity])],
   controllers: [FileUploadController],
-  providers: [FileUploadService]
+  providers: [FileUploadService,]
 })
 export class FileUploadModule {}
